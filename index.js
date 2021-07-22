1d24892b762182350fc959f1e1a5ddd6162abf20
Ricardo Madrigal <d3v1an.tux@gmail.com>
2021-07-22 17:45:05 -0500

    Integracion de axios

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios').create({
+    baseURL: "https://hcd.urbanissa.net"
+    // headers: {
+    //     'apikey': process.env.SMS_API_KEY
+    // }
+});
+
+const ax = async () => {
+    return await axios.post('/api/cd/exec', {
+        'message': `Prueba con axion`
+    });
+}
+// gp().then(d => {
+//     console.log(d);
+// })
+// .catch(e => {
+//     console.log(e);
+// });
 
 try {
+    ax().then(d => console.log('Axios', d) ).catch(e => console.log('Error', e));
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
