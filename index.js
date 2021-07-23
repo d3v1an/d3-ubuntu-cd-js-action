e9bc7d45899b4681215958bb2b3d8d0eb90d9fbc
Ricardo Madrigal <d3v1an.tux@gmail.com>
2021-07-22 21:14:31 -0500

    Axios.

--- index.js
+++ index.js
@@ -1,38 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const https = require('https'); // https://hcd.urbanissa.net/api/cd/exec
+// const https = require('https'); // https://hcd.urbanissa.net/api/cd/exec
+const axios = require('axios');
 
 try {
 
-    const options = {
-        host: 'hcd.urbanissa.net',
-        port: 443,
-        path: '/api/cd/exec',
-        method: 'POST',
-        headers: {
-          'Accept': 'application/vnd.github.everest-preview+json',
-          'Content-Type': 'application/json; charset=UTF-8'
-        }
-    };
+    // const options = {
+    //     host: 'hcd.urbanissa.net',
+    //     port: 443,
+    //     path: '/api/cd/exec',
+    //     method: 'POST',
+    //     headers: {
+    //       'Accept': 'application/vnd.github.everest-preview+json',
+    //       'Content-Type': 'application/json; charset=UTF-8'
+    //     }
+    // };
 
-    const request = https.request(options, (res) => {
-        
-        let data = '';
-
-        res.on('data', (chunk) => {
-            data += chunk;
-        });
-
-        res.on('close', () => {
-            console.log('Added new test');
-            console.log(JSON.parse(data));
-        });
-    });
-
-    request.on('error', (e) => {
-        console.error(e);
-      });
-    request.end();
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
@@ -41,6 +24,19 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  axios
+  .post('https://hcd.urbanissa.net/api/cd/exec', {
+    todo: 'Buy the milk'
+  })
+  .then(res => {
+    console.log(`statusCode: ${res.statusCode}`)
+    console.log(res)
+  })
+  .catch(error => {
+    console.error(error)
+  });
+
 } catch (error) {
   core.setFailed(error.message);
 }
